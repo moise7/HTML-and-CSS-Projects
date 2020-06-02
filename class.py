2336aff5face6b06d71decc6c5d9605f20f2d389
moise7 <63320773+moise7@users.noreply.github.com>
2020-06-02 15:36:46 -0400

    Add files via upload

---
+++ class.py
@@ -0,0 +1,8 @@
+class Car_user:
+    name = 'No Name Provided'
+    email = ''
+    password = '1234'
+    acount_id = 0
+class Customer(Car_user):
+    car_payment = 119.00
+    mailing_address = ' '
