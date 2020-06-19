a6dacac3afdfa012ea02195bfc4a6e224ddf747d
moise7 <63320773+moise7@users.noreply.github.com>
2020-06-19 15:04:20 -0400

    Add files via upload

--- Transfer Assig.py
+++ Transfer Assig.py
@@ -1,11 +1,14 @@
 import shutil
 import os
 import time
+from tkinter import*
+import tkinter as tk
+from tkinter import messagebox
 
 #set where the source of the file are
-source = 'C:\\Users\\MBNewVision\\Desktop\A
+source = 'C:\python_projects\Transfer\A'
 
-destination = 'C:\Users\MBNewVision\Desktop\B
+destination = 'C:\python_projects\Transfer\B'
 files = os.listdir(source)
 
 for i in files:
@@ -16,3 +19,21 @@ print("modified in the last 24 hours",access_time)
 
 local_time = time.ctime(modification_time)
 print("Last modification time(Local time):",local_time)
+
+#Creating UI that contain the files to be checked daily
+window=Tk()
+var = StringVar()
+var.set("Daily")
+data=('C:\python_projects\Transfer\A')
+mgb = messagebox(windows, values=data)
+mgb.place(x=60, y=150)
+#Creating UI for Receiving and file checking
+v0=IntVar()
+v0.set(1)
+r1=Radiobutton(window, text="Receive", variable=v0,value=1)
+r2=Radiobutton(window, text="File Check", variable=v0,value=2)
+
+
+
+if __name__ == "__main__main":
+    pass()
