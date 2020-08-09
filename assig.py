3a717879685accf1abc929e511137a94a83e2cb8
moise7 <63320773+moise7@users.noreply.github.com>
2020-08-08 23:12:47 -0400

    Add files via upload

--- assig.py
+++ assig.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.Connection('name.db')
+conn = sqlite3.Connection("name.db")
 
 with conn:
     cur = conn.cursor()
@@ -11,22 +11,22 @@ with conn:
             col_document TEXT\
                ) ")
     conn.commit()
-
+conn.close()
 
 conn = sqlite3.connect('name.db')
 fileList = ['information.docx','Hello.txt','myImage.png','myMovie.mpg','World.txt','data.pdf','myPhoto.jpg']
 for name in fileList:
-     if (name.endswith("txt.")):
-          print(name)
-          cur.execute("INSERT INTO tbl_list(col_document)VALUES(?)",(str.split(name)))
-     
+     if name.endswith("txt"):
+     print(name)
+     cur.execute("INSERT INTO tbl_list(col_document)VALUES(?)",(str.split(name)))
      conn.commit()
 conn.close()
 
 conn = sqlite3.connect('name.db')
 with conn:
     cur = conn.cursor()
-    cur.execute("SELECT .txt, [FROM tbl_list] * [col_document] = 'name'")
+    cur.execute("SELECT .txt, FROM tbl_list= 'name'")
+    print("txt file",(col_document,txt))
     conn.commit()
 conn.close()
    
